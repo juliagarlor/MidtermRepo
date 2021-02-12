d3ae8fa11b0c8f9c9ae866763fa94e4b1e90a6db
juliagarcia <julia.garcia.lorente@gmail.com>
2021-02-12 23:49:54 +0100

    Added sql queries to populate databases

--- src/test/resources/static/superdbTestQueries.sql
+++ src/test/resources/static/superdbTestQueries.sql
@@ -1,117 +1,17 @@
-DROP SCHEMA IF EXISTS superdb_test;
-CREATE SCHEMA superdb_test;
-USE superdb_test;
+INSERT INTO `superdb_test`.`user` (`id`, `age`, `first_name`, `last_name`, `salutation`, `password`) VALUES ('1', '40', 'maria', 'de todos los santos', 'Mrs', '$2a$10$DfHDRvoio1Km7RwWTTZGR.fHNF0NuiJMlYwz8T6Un2GirK.fk6PE.');
+INSERT INTO `superdb_test`.`user` (`id`, `age`, `first_name`, `last_name`, `salutation`, `password`) VALUES ('2', '20', 'cayetano', 'de todos los santos', 'Mr', '$2a$10$DfHDRvoio1Km7RwWTTZGR.fHNF0NuiJMlYwz8T6Un2GirK.fk6PE.');
 
-CREATE TABLE `user`(
-id BIGINT AUTO_INCREMENT NOT NULL,
-age INT,
-`password` VARCHAR(255),
-last_name VARCHAR(60),
-first_name VARCHAR(60),
-middle_name VARCHAR(60),
-salutation VARCHAR(60),
-PRIMARY KEY(id)
-);
+INSERT INTO `superdb_test`.`account_holder` (`date_of_birth`, `mailing_address_city`, `mailing_address_country`, `mailing_address_number`, `mailing_address_street`, `primary_address_city`, `primary_address_country`, `primary_address_number`, `primary_address_street`, `id`) VALUES ('1998-12-12 00:00:00.000000', 'city1', 'country1', '1', 'street1', 'city2', 'country2', '2', 'street2', '1');
+INSERT INTO `superdb_test`.`account_holder` (`date_of_birth`, `mailing_address_city`, `mailing_address_country`, `mailing_address_number`, `mailing_address_street`, `primary_address_city`, `primary_address_country`, `primary_address_number`, `primary_address_street`, `id`) VALUES ('1980-08-09', 'city1', 'country1', '1', 'street1', 'city2', 'country2', '2', 'street2', '2');
 
-CREATE TABLE account_holder(
-id BIGINT AUTO_INCREMENT NOT NULL,
-date_of_birth DATETIME,
-mailing_address_number INT,
-mailing_address_street VARCHAR(60),
-mailing_address_city VARCHAR(60),
-mailing_address_country VARCHAR(60),
-primary_address_number INT,
-primary_address_street VARCHAR(60),
-primary_address_city VARCHAR(60),
-primary_address_country VARCHAR(60),
-PRIMARY KEY(id),
-FOREIGN KEY(id) REFERENCES `user`(id)
-);
+INSERT INTO `superdb_test`.`role` (`id`, `name`, `user_id`) VALUES ('1', 'ACCOUNT_HOLDER', '1');
+INSERT INTO `superdb_test`.`role` (`id`, `name`, `user_id`) VALUES ('2', 'ACCOUNT_HOLDER', '2');
 
-CREATE TABLE `admin`(
-id BIGINT AUTO_INCREMENT NOT NULL,
-PRIMARY KEY(id),
-FOREIGN KEY(id) REFERENCES `user`(id)
-);
+INSERT INTO `superdb_test`.`account` (`id`, `amount`, `currency`, `balance`, `status`, `primary_owner_id`) VALUES ('1', '0.00', 'USD', '1000.00', 'ACTIVE', '1');
+INSERT INTO `superdb_test`.`account` (`id`, `amount`, `currency`, `balance`, `status`, `primary_owner_id`) VALUES ('2', '0.00', 'USD', '1500.00', 'ACTIVE', '2');
 
-CREATE TABLE third_party(
-id BIGINT AUTO_INCREMENT NOT NULL,
-hashed_key VARCHAR(255),
-PRIMARY KEY(id),
-FOREIGN KEY(id) REFERENCES `user`(id)
-);
+INSERT INTO `superdb_test`.`transactions` (`amount`, `currency`, `moment`, `emisor_id`, `receptor_id`) VALUES ('50', '0', '2021-02-11-12-12-01', '1', '2');
+INSERT INTO `superdb_test`.`transactions` (`amount`, `currency`, `moment`, `emisor_id`, `receptor_id`) VALUES ('20', '0', '2021-02-11-12-12-01', '1', '2');
 
-CREATE TABLE `account`(
-id BIGINT AUTO_INCREMENT NOT NULL,
--- ni puñetera idea de por que me obliga a poner aqui amount
-amount DECIMAL(19, 4),
-balance DECIMAL(19, 4),
-currency VARCHAR(255),
-primary_owner_id BIGINT,
-secondary_owner_id BIGINT,
-PRIMARY KEY(id),
-FOREIGN KEY(primary_owner_id) REFERENCES account_holder(id),
-FOREIGN KEY(secondary_owner_id) REFERENCES account_holder(id)
-);
-
-CREATE TABLE checking_account(
-id BIGINT AUTO_INCREMENT NOT NULL,
-amount DECIMAL(19, 4),
-currency VARCHAR(255),
-secret_key VARCHAR(255),
-`status` VARCHAR(255),
-PRIMARY KEY(id),
-FOREIGN KEY(id) REFERENCES `account`(id)
-);
-
-CREATE TABLE credit_card_account(
-id BIGINT AUTO_INCREMENT NOT NULL,
-currency VARCHAR(255),
-monthly_maintenance_fee DECIMAL(19, 4),
-interest_rate DECIMAL(19, 4),
-credit_limit DECIMAL(19, 4),
-PRIMARY KEY(id),
-FOREIGN KEY(id) REFERENCES `account`(id)
-);
-
-CREATE TABLE savings_account(
-id BIGINT AUTO_INCREMENT NOT NULL,
--- amount DECIMAL(19, 4),
-currency VARCHAR(255),
-secret_key VARCHAR(255),
-`status` VARCHAR(255),
-minimum_balance DECIMAL(19, 4),
-interest_rate DECIMAL(19, 4),
-PRIMARY KEY(id),
-FOREIGN KEY(id) REFERENCES `account`(id)
-);
-
-CREATE TABLE student_checking_account(
-id BIGINT AUTO_INCREMENT NOT NULL,
--- amount DECIMAL(19, 4),
--- currency VARCHAR(255),
-secret_key VARCHAR(255),
-`status` VARCHAR(255),
-PRIMARY KEY(id),
-FOREIGN KEY(id) REFERENCES `account`(id)
-);
-
-CREATE TABLE transactions(
-id BIGINT AUTO_INCREMENT NOT NULL,
-emisor_id BIGINT,
-receptor_id BIGINT,
-amount DECIMAL(19, 4),
-currency VARCHAR(255),
-moment DATETIME,
-PRIMARY KEY(id),
-FOREIGN KEY(emisor_id) REFERENCES `account`(id),
-FOREIGN KEY(receptor_id) REFERENCES `account`(id)
-);
-
-CREATE TABLE `role` (
-  id BIGINT AUTO_INCREMENT NOT NULL,
-  `name` VARCHAR(255),
-  user_id BIGINT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES `user`(id)
-);
+INSERT INTO `superdb_test`.`checking_account` (`amount`, `currency`, `secret_key`, `id`) VALUES ('0.00', 'USD', '1234', '1');
+INSERT INTO `superdb_test`.`checking_account` (`amount`, `currency`, `secret_key`, `id`) VALUES ('0.00', 'USD', '1234', '2');
